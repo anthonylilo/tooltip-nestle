d754170652e82c629d34e3dba67fb9df33882af3
anthonylilo <ammen0203@gmail.com>
2023-08-26 18:00:02 -0400

    fixed drupal duplication issue

--- themes/glossary/js/custom.js
+++ themes/glossary/js/custom.js
@@ -1,58 +1,46 @@
-(function ($, Drupal) {
-  Drupal.behaviors.glossaryTooltip = {
-    attach: function (context, settings) {
-      $(document).ready(function () {
-        $.ajax({
-          url: "http://localhost/paginas/nestle/get-glossary",
-          success: function (data) {
-            const wordsInDocument = $(".layout-content").text().split(/\s+/);
-            const titles = data.map((entry) => entry.title);
-            const filterMatchingWords = (compareArray, documentWords) => {
-              let result = [];
-              compareArray.forEach((entry) => {
-                result.push(
-                  documentWords.filter((word) => {
-                    return (
-                      word.toLowerCase() ===
-                      entry.toLowerCase().replace(".", "")
-                    );
-                  })[0]
-                );
-              });
-              return result;
-            };
-            const matchingWords = filterMatchingWords(titles, wordsInDocument);
-            const description = data.map((entry) => entry.description);
-            const id = data.map((entry) => entry.id);
-            const placeTooltip = (array, text) => {
-              let newText = text;
-              array.forEach((word, index) => {
-                const regex = new RegExp(`\\b${word}\\b`);
-                newText = newText.replace(regex, (match) => {
-                  const descriptionText = description[index];
-                  let tooltipContent = descriptionText;
+jQuery(document).ready(function ($) {
+  $.ajax({
+    //PUT YOUR LOCAL PATH
+    url: "http://localhost/paginas/nestle/get-glossary",
+    success: function (data) {
+      const wordsInDocument = $("#block-glossary-content").text().split(/\s+/);
 
-                  if (descriptionText.length > 100) {
-                    tooltipContent =
-                      descriptionText.substring(0, 100) +
-                      ' <span class="read-more">' +
-                      "Read more</span>";
-                  }
-
-                  return `<div class="tooltip"><a href="http://localhost/paginas/nestle/glossary-term/${id[index]}">${match}<span class="tooltiptext">${tooltipContent}</span></a></div>`;
-                });
-              });
-              return newText;
-            };
-            let content = $(".layout-content").html();
-            content = placeTooltip(matchingWords, content);
-            $(".layout-content").html(content);
-          },
-          error: function (xhr, status, error) {
-            console.log("Error AJAX:", error);
-          },
+      const filterMatchingWords = (compareArray, documentWords) => {
+        let result = [];
+        compareArray.forEach((entry) => {
+          const filteredTitle = documentWords.filter((word) => {
+            return (
+              word.toLowerCase() === entry.title.toLowerCase().replace(".", "")
+            );
+          })[0];
+          if (filteredTitle) result.push({ ...entry, title: filteredTitle });
         });
-      });
+        return result;
+      };
+
+      const matchingWords = filterMatchingWords(data, wordsInDocument);
+
+      const placeTooltip = (array, text) => {
+        array.forEach((item) => {
+          const regex = new RegExp(`\\b${item.title}\\b`, "gi");
+          const tooltipContent =
+            item.description.length > 100
+              ? item.description.substring(0, 100) +
+                " <span class='read-more'>Read more...</span>"
+              : item.description;
+          text = text.replace(regex, (match) => {
+            return `<div class="tooltip"><a href="http://localhost/paginas/nestle/glossary-term/${item.id}">${match}<span class="tooltiptext">${tooltipContent}</span></a></div>`;
+          });
+        });
+        return text;
+      };
+
+      let content = $("#block-glossary-content").html();
+      content = placeTooltip(matchingWords, content);
+      $("#block-glossary-content").html(content);
     },
-  };
-})(jQuery, Drupal);
+    error: function (xhr, status, error) {
+      console.log("Error AJAX:", error);
+    },
+  });
+});
